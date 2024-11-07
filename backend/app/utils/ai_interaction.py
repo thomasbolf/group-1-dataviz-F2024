029221c0e4ca2a101a12d735dd02217fa08c1ddf
thomasbolf <thomasbolf1@tamu.edu>
2024-11-06 23:14:19 -0600

    commented out code that used outdated chatgpt package

--- backend/app/utils/ai_interaction.py
+++ backend/app/utils/ai_interaction.py
@@ -18,18 +18,18 @@ def wait_on_run(run, thread):
         )
         time.sleep(10)
     return run
-def get_insights(prompt):
-    openai.api_key = os.getenv("OPENAI_API_KEY")
+# def get_insights(prompt):
+#     openai.api_key = os.getenv("OPENAI_API_KEY")
 
-    response = openai.ChatCompletion.create(
-        model="gpt-4",  # Using GPT-4
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": prompt}
-        ],
-        max_tokens=150
-    )
-    return response['choices'][0]['message']['content'].strip()
+#     response = openai.ChatCompletion.create(
+#         model="gpt-4",  # Using GPT-4
+#         messages=[
+#             {"role": "system", "content": "You are a helpful assistant."},
+#             {"role": "user", "content": prompt}
+#         ],
+#         max_tokens=150
+#     )
+#     return response['choices'][0]['message']['content'].strip()
 def generate_code_and_graph(data, prompt):
     initial_instructions = """
         You are working on an automated visualization system that generates customizable visualizations using Python’s Matplotlib library. 
